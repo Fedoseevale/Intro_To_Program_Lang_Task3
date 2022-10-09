54f1643f708d73281f34ffdd0fff0912f47e7423
Александр <fedoseevale@gmail.com>
2022-10-09 14:54:14 +0400

    Ex19 done

--- Execise19/Program.cs
+++ Execise19/Program.cs
@@ -3,5 +3,20 @@
 //является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
-// // 23432 -> да
+// 23432 -> да
 
+Console.WriteLine ("Введите пятизначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+void Number (int num)
+{
+    int num1 = num / 10000;
+    int num2 = num / 1000 % 10;
+    int num4 = num / 10 % 10;
+    int num5 = num % 10;
+    if (num > 99999 || num < 10000 || num == 0) Console.WriteLine ("Введённое число не является пятизначным");
+    else if (num1 == num5 && num2 == num4) Console.WriteLine ("Введённое число является полиндромом");
+    else Console.WriteLine ("Введённое число не является полиндромом");
+}
+
+Number(number);
